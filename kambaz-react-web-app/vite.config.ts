22f04460e6c7f8b342d6effa5798d47abcdf44c4
SFA24SCM90S <121506217+Satwik-Sinha@users.noreply.github.com>
2025-08-06 14:33:57 -0400

    Initial commit

---
+++ kambaz-react-web-app/vite.config.ts
@@ -0,0 +1,23 @@
+// vite.config.ts
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        app: 'app.html'
+      }
+    }
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:4000',
+        changeOrigin: true,
+      },
+    },
+  },
+});
